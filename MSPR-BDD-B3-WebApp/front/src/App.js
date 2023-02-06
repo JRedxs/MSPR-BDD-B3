a2644611262752acedbbcdc39941f7a663f9fdf3
ViTj4 <remi.ryckebusch.pro@gmail.com>
2023-02-06 11:18:15 +0100

    Commit ajout des routes et refonte brèves de la page de login

--- MSPR-BDD-B3-WebApp/front/src/App.js
+++ MSPR-BDD-B3-WebApp/front/src/App.js
@@ -1,13 +1,26 @@
-import logo from './logo.svg';
 import './App.css';
-import LoginPage from './components/Login';
 
-function App() {
+import LoginPage             from './components/Login';
+import Map                   from './components/Map';
+import NotFound              from './components/NotFound'; 
+import {Routes, Route, Link} from 'react-router-dom';
+
+const App = () => {
   return (
-    <div className="App">
-      <LoginPage></LoginPage>
-    </div>
+      <div>
+          <nav className="navbar navbar-expand-lg navbar-light bg-primary">
+          <ul className="navbar-nav mr-auto">
+            <li><Link to="/" className="nav-link"> Login </Link></li>
+            <li><Link to="/Map" className="nav-link">Map</Link></li>
+          </ul>
+          </nav>
+          <hr />
+          <Routes>
+            <Route exact path="" element={<LoginPage/>} />
+            <Route path="Map" element={<Map/>} />
+            <Route path="*" element={<NotFound/>}/>
+          </Routes>
+        </div>
   );
 }
-
 export default App;
--- MSPR-BDD-B3-WebApp/front/src/components/Login.js
+++ MSPR-BDD-B3-WebApp/front/src/components/Login.js
@@ -1,32 +1,38 @@
 import React from "react";
-// import { TextInput } from "react-native";
 import '../styles/Login.css';
 
 
-function LoginPage(){
+const LoginPage = () => {
 
     //front commencé pour maintenant effectuer les tests de connexion / regex du mdp et email / button login
 
     return(
         <>
-            <div className="name-company">
-                <h1>A'Rosa-Je</h1>
-            </div>
-
-            <div className="box-login">
-                <div className="input-mail">
-                    <input placeholder="e-mail" type="text"></input>
-                </div>
-                <br/>
-                <div className="input-password">
-                    <input placeholder="password" type="text"></input>
-                </div>
-                <br/>
-                <div className="button-login">
-                    <button>Login</button>
+            <div className="justify-content-center">
+                <div className="card text-center ">
+                    <div className="card-body">
+                        <h1 className="card-title">A'Rosa-Je</h1>
+                    </div>
                 </div>
             </div>
-
+            <div className="justify-content-center">
+                <form>
+                    <div className="mb-3">
+                        <label for="exampleInputEmail1" className="form-label">Email address</label>
+                        <input type="email" className="form-control" id="exampleInputEmail1" aria-describedby="emailHelp"/>
+                        <div id="emailHelp" className="form-text">We'll never share your email with anyone else.</div>
+                    </div>
+                    <div className="mb-3">
+                        <label for="exampleInputPassword1" className="form-label">Password</label>
+                        <input type="password" className="form-control" id="exampleInputPassword1"/>
+                    </div>
+                    <div className="mb-3 form-check">
+                        <input type="checkbox" className="form-check-input" id="exampleCheck1"/>
+                        <label className="form-check-label" for="exampleCheck1">Check me out</label>
+                    </div>
+                    <button type="submit" className="btn btn-primary">Submit</button>
+                </form>
+            </div>
         </>
     )
 }
