d8db4ab48d2c333737d7273d65ac7242409dff5b
McLouffy <119299314+McLouffy@users.noreply.github.com>
2023-02-07 14:57:30 +0100

    maj return bdd

--- MSPR-BDD-B3-Backend/back/src/main.py
+++ MSPR-BDD-B3-Backend/back/src/main.py
@@ -17,7 +17,7 @@ def get_users():
             result = cursor.fetchall()
             users = []
             for row in result:
-                users.append({"id_person": row[0], "name": row[1], "firstname": row[2]})
+                users.append({"id_person": row[0], "name": row[1], "firstname": row[2], "pwd": row[3], "email": row[4], "phone": row[5], "latitude": row[6], "longitude": row[7], "id_role": row[8]})
             return {"Person": users}
     except:
         return {"erreur": "aucun user récupéré"}, 500
@@ -25,9 +25,10 @@ def get_users():
 @app.get("/users/{user_id}")
 def get_user_by_id(user_id: int):
     cursor = connection.cursor()
-
     sql = "SELECT * FROM Person WHERE id_person=%s"
-
     cursor.execute(sql, (user_id,))
-    result = cursor.fetchone()
-    return {"Person": result}
+    result = cursor.fetchall()
+    users = []
+    for row in result:
+        users.append({"id_person": row[0], "name": row[1], "firstname": row[2], "pwd": row[3], "email": row[4], "phone": row[5], "latitude": row[6], "longitude": row[7], "id_role": row[8]})
+    return {"Person": users}
