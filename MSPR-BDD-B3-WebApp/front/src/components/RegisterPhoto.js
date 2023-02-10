b2a500031ce01f501e848daa3bce8617f643bf63
ViTj4 <remi.ryckebusch.pro@gmail.com>
2023-02-10 12:05:33 +0100

    mise en page interface pour enregistrer une nouvelle photo

--- MSPR-BDD-B3-WebApp/front/src/components/RegisterPhoto.js
+++ MSPR-BDD-B3-WebApp/front/src/components/RegisterPhoto.js
@@ -1,4 +1,7 @@
 import React, { useState, useRef } from 'react';
+import logo from '../assets/images/logo.png'
+
+
 
 const RegisterPhoto = () => {
   const [isCameraActive, setIsCameraActive] = useState(false);
@@ -44,29 +47,53 @@ const RegisterPhoto = () => {
     <div>
       {
         isCameraActive && (
-          <>
-            <video ref={videoRef} autoPlay />
-            <button onClick={handleTakePhoto}>Prendre une Photo</button>
-            <button onClick={handleStopCamera}>Arreter la Camera</button>
-          </>
+          <div className="d-flex justify-content-center margin-login-card" style={{margin:"5em"}}>
+            <div className="card">
+              <video ref={videoRef} autoPlay />
+              <div className="card-body">
+                <div className="d-flex justify-content-center align-items-center">
+                  <b><p className="card-text">Vous pouvez enregistrer ici une nouvelle photo pour votre plante</p></b>
+                </div>
+                <div className="d-flex justify-content-center align-items-center">
+                  <button className='btn btn-success' style={{margin:"1em"}} onClick={handleTakePhoto}>Prendre une Photo</button>
+                  <button className='btn btn-success' style={{margin:"1em"}} onClick={handleStopCamera}>Arreter la Camera</button>
+                </div>
+              </div>
+            </div> 
+          </div>
         )
       }
       {
         !isCameraActive && !imageSrc && (
-          <button onClick={handleStartCamera}>Lancer la Camera</button>
+          <div className="d-flex justify-content-center align-items-center" style={{margin:"5em"}}>
+            <div className="card  d-flex justify-content-center align-items-center">
+            <img src={logo} class="card-img-top" alt="logo"/>
+              <div className="card-body ">
+                <b><p className="card-text">Vous pouvez enregistrer ici une nouvelle photo pour votre plante</p></b>
+              </div>
+                <div className="d-flex justify-content-center">
+                  <button className='btn btn-success' style={{margin:"2em"}} onClick={handleStartCamera}>Lancer la Camera</button>
+                </div>
+              </div>
+            </div> 
         )
       }
       {
         imageSrc && (
-          <>
-            <img src={imageSrc} />
-            <button onClick={handleUploadPhoto}>Enregister la Photo</button>
-            <button onClick={handleTakeNewPhoto}>Prendre une nouvelle Photo</button>
-          </>
+          <div className="d-flex justify-content-center align-items-center"  >
+            <div className="card" style={{margin:"5em"}}>
+              <img src={imageSrc} alt="" />
+              <div className="card-body">
+                <div className="d-flex justify-content-center align-items-center"> 
+                  <button onClick={handleUploadPhoto}>Enregister la Photo</button>
+                  <button onClick={handleTakeNewPhoto}>Prendre une nouvelle Photo</button>
+                </div>
+              </div>
+            </div> 
+          </div>
         )
       }
     </div>
   );
 };
-
 export default RegisterPhoto;
