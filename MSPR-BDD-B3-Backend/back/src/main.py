5cef6ed9e946d503d7570a913e56a433b601cb3f
JRedxs <98217327+JRedxs@users.noreply.github.com>
2023-02-10 15:11:27 +0100

    update du main et du login

--- MSPR-BDD-B3-Backend/back/src/main.py
+++ MSPR-BDD-B3-Backend/back/src/main.py
@@ -22,39 +22,38 @@ app.add_middleware(
     allow_headers=["*"],
 )
 
-
-# Créez la route pour sélectionner tous les utilisateurs
 @app.get("/users")
-def get_users():
+def get_user(email: str, password: str):
     try:
         with connection.cursor() as cursor:
-            sql = "SELECT * FROM Person"
-            cursor.execute(sql)
+            sql = "SELECT * FROM Person WHERE email=%s and pwd=%s"
+            cursor.execute(sql, (email, password))
             result = cursor.fetchall()
             users = []
             for row in result:
                 users.append({"id_person": row[0], "name": row[1], "firstname": row[2], "pwd": row[3], "email": row[4], "phone": row[5], "latitude": row[6], "longitude": row[7], "id_role": row[8]})
-            return {"Person": users}
+            if users:
+                return {"User": users}
+            else:
+                raise HTTPException(status_code=400, detail="Incorrect email or password")
     except:
-        return {"erreur": "aucun user récupéré"}, 500
+        raise HTTPException(status_code=500, detail="Database connection error")
+
 
 @app.get("/users/{user_id}")
 def get_user_by_id(user_id: int):
-    cursor = connection.cursor()
-    sql = "SELECT * FROM Person WHERE id_person=%s"
-    cursor.execute(sql, (user_id,))
-    result = cursor.fetchall()
-    users = []
-    for row in result:
-        users.append({"id_person": row[0], "name": row[1], "firstname": row[2], "pwd": row[3], "email": row[4], "phone": row[5], "latitude": row[6], "longitude": row[7], "id_role": row[8]})
-    return {"Person": users}
-
-
-
-
-@app.options("/persons")
-async def add_user_options(person: Person):
-    return {"Allow": "POST"}
+    try :
+        with connection.cursor() as cursor:
+            sql = "SELECT * FROM Person WHERE id_person=%s"
+            cursor.execute(sql, (user_id,))
+            result = cursor.fetchall()
+            users = []
+            for row in result:
+                users.append({"id_person": row[0], "name": row[1], "firstname": row[2], "pwd": row[3], "email": row[4], "phone": row[5], "latitude": row[6], "longitude": row[7], "id_role": row[8]})
+                return {"Person": users}
+    except:
+            return {"Personne inexistante"}
+        
 
 
 @app.post("/persons")
@@ -72,3 +71,7 @@ async def add_user(person: Person):
     return {"message": "Ajout avec succès"}
 
       
+
+#### ROUTE DE DEV ####
+
+# Créez la route pour sélectionner tous les utilisateurs
