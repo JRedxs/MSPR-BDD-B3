38b38877b82523f478bd25cc91145d547e1b679f
JRedxs <98217327+JRedxs@users.noreply.github.com>
2023-02-10 09:56:17 +0100

    ajout des users

--- MSPR-BDD-B3-Backend/back/src/main.py
+++ MSPR-BDD-B3-Backend/back/src/main.py
@@ -1,4 +1,5 @@
-from fastapi import FastAPI
+from fastapi import FastAPI, HTTPException
+from pydantic import BaseModel
 import pymysql
 
 # Connexion à la base de données
@@ -6,6 +7,17 @@ connection = pymysql.connect(host='localhost', user='admin', password='admin', d
 
 # Initialisez l'application 
 app = FastAPI()
+
+
+
+class Person(BaseModel):
+        name: str
+        firstname: str
+        password: str
+        email: str
+        phone: str
+        id_role: int
+
 
 # Créez la route pour sélectionner tous les utilisateurs
 @app.get("/users")
@@ -32,3 +44,24 @@ def get_user_by_id(user_id: int):
     for row in result:
         users.append({"id_person": row[0], "name": row[1], "firstname": row[2], "pwd": row[3], "email": row[4], "phone": row[5], "latitude": row[6], "longitude": row[7], "id_role": row[8]})
     return {"Person": users}
+
+
+
+
+
+
+@app.post("/persons")
+async def add_user(person: Person):
+    cursor = connection.cursor()
+    cursor.execute("SELECT * FROM Person WHERE email=%s", (person.email,))
+    if cursor.fetchone() is not None:
+        raise HTTPException(status_code=409, detail="Email déjà existants")
+    
+    insert_query = "INSERT INTO Person (name, firstname, pwd, email, phone,id_role) VALUES (%s, %s, %s, %s,%s,%s)"
+    cursor.execute(insert_query, (person.name, person.firstname, person.password, person.email, person.phone,person.id_role))
+    connection.commit()
+    cursor.close()
+
+    return {"message": "Ajout avec succès"}
+
+      
