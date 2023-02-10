06e5149a16c8d561d5603fb79ead988643dda679
ViTj4 <remi.ryckebusch.pro@gmail.com>
2023-02-10 09:10:26 +0100

    WORK IN PROGRESS

--- MSPR-BDD-B3-WebApp/front/src/components/Register.js
+++ MSPR-BDD-B3-WebApp/front/src/components/Register.js
@@ -19,78 +19,76 @@ const Register = () => {
     return (
         <>  
         <div className="d-flex align-items-center justify-content-center">
-        <div className="card  card-color w-25 d-flex align-items-center justify-content-center">
-                <form onSubmit={handleSubmit}>
-                    <div>
-                        <label className="form_label" htmlFor="firstName">Prénom :</label>
-                            <input
-                                className="form-control m-2 w-auto"
-                                type="text"
-                                id="firstName"
-                                value={firstName}
-                                onChange={(event) => setFirstName(event.target.value)}
-                            />
-                    </div>
+            <div className="card  card-color w-25 d-flex align-items-center justify-content-center">
+                    <form onSubmit={handleSubmit}>
                         <div>
-                            <label htmlFor="lastName">Nom :</label>
-                            <input
-                                className="form-control m-2 w-auto"
-                                type="text"
-                                id="lastName"
-                                value={lastName}
-                                onChange={(event) => setLastName(event.target.value)}
-                            />
+                            <label className="form_label" htmlFor="firstName">Prénom :</label>
+                                <input
+                                    className="form-control m-2 w-auto"
+                                    type="text"
+                                    id="firstName"
+                                    value={firstName}
+                                    onChange={(event) => setFirstName(event.target.value)}
+                                />
                         </div>
-                        <div>
-                            <label htmlFor="phone">Téléphone :</label>
-                            <input
-                                className="form-control m-2 w-auto"
-                                type="tel"
-                                id="phone"
-                                value={phone}
-                                onChange={(event) => setPhone(event.target.value)}
-                            />
-                        </div>
-                        <div>
-                            <label htmlFor="email">Email :</label>
-                            <input
-                                className="form-control m-2 w-auto"
-                                type="email"
-                                id="email"
-                                value={email}
-                                onChange={(event) => setEmail(event.target.value)}
-                            />
-                        </div>
-                        <div>
-                            <label htmlFor="password">Password :</label>
-                            <input
-                                className="form-control m-2 w-auto"
-                                type="password"
-                                id="password"
-                                value={password}
-                                onChange={(event) => setPassword(event.target.value)}
-                            />
-                        </div>
-                        <div>
-                        <label htmlFor="confirmPassword">Confirmer password :</label>
-                            <input
-                                className="form-control m-2 w-auto"
-                                type="password"
-                                id="confirmPassword"
-                                value={confirmPassword}
-                                onChange={(event) => setConfirmPassword(event.target.value)}
-                            />
-                        </div>
-                        <div className="d-flex align-items-center justify-content-center">
-                            <button className="btn btn-dark" type="submit">S'inscrire</button>
-                        </div>
-                </form>
+                            <div>
+                                <label htmlFor="lastName">Nom :</label>
+                                <input
+                                    className="form-control m-2 w-auto"
+                                    type="text"
+                                    id="lastName"
+                                    value={lastName}
+                                    onChange={(event) => setLastName(event.target.value)}
+                                />
+                            </div>
+                            <div>
+                                <label htmlFor="phone">Téléphone :</label>
+                                <input
+                                    className="form-control m-2 w-auto"
+                                    type="tel"
+                                    id="phone"
+                                    value={phone}
+                                    onChange={(event) => setPhone(event.target.value)}
+                                />
+                            </div>
+                            <div>
+                                <label htmlFor="email">Email :</label>
+                                <input
+                                    className="form-control m-2 w-auto"
+                                    type="email"
+                                    id="email"
+                                    value={email}
+                                    onChange={(event) => setEmail(event.target.value)}
+                                />
+                            </div>
+                            <div>
+                                <label htmlFor="password">Password :</label>
+                                <input
+                                    className="form-control m-2 w-auto"
+                                    type="password"
+                                    id="password"
+                                    value={password}
+                                    onChange={(event) => setPassword(event.target.value)}
+                                />
+                            </div>
+                            <div>
+                            <label htmlFor="confirmPassword">Confirmer password :</label>
+                                <input
+                                    className="form-control m-2 w-auto"
+                                    type="password"
+                                    id="confirmPassword"
+                                    value={confirmPassword}
+                                    onChange={(event) => setConfirmPassword(event.target.value)}
+                                />
+                            </div>
+                            <div className="d-flex align-items-center justify-content-center">
+                                <button className="btn btn-dark" type="submit">S'inscrire</button>
+                            </div>
+                    </form>
+                </div>
             </div>
-        </div>
-
         </>
     )
 }
-
 export default Register;
 
