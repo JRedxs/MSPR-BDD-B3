5d6e6c5624aeed07ef6b53d66a478e4e7113b071
Vonkuro <vonkuro@gmail.com>
2023-02-10 14:20:06 +0100

    get and post route of image is done

--- MSPR-BDD-B3-Backend/back/src/main.py
+++ MSPR-BDD-B3-Backend/back/src/main.py
@@ -47,6 +47,7 @@ def get_user_by_id(user_id: int):
     users = []
     for row in result:
         users.append({"id_person": row[0], "name": row[1], "firstname": row[2], "pwd": row[3], "email": row[4], "phone": row[5], "latitude": row[6], "longitude": row[7], "id_role": row[8]})
+    cursor.close()
     return {"Person": users}
 
 
@@ -68,34 +69,39 @@ async def add_user(person: Person):
     cursor.execute(insert_query, (person.name, person.firstname, person.password, person.email, person.phone,person.id_role))
     connection.commit()
 
-
+    cursor.close()
     return {"message": "Ajout avec succès"}
 
       
 
 @app.post("/image")
-def register_image(image: NewImage):
+async def register_image(image: NewImage):
     request_select = "select id_plante from Plante where id_plante = %s limit 1;"
-    request_insert = "insert into Photo (image_data) values(%s);"
+    request_insert = "insert into Photo (image_data, id_plante) values(%s,%s);"
 
     cursor = connection.cursor()
     cursor.execute(request_select, (image.id_plante))
     if cursor.fetchone() is None:
         raise HTTPException(status_code=404, detail="Photo pour une plante inexistante")
     
-    cursor.execute(request_insert, (image.data))
+    cursor.execute(request_insert, (image.data,image.id_plante))
     connection.commit()
     cursor.close()
     return {"message": "Photo enregistrée"}
     
 @app.get("/image/{id}", response_model=DBImage)
 def send_image(id):
-    request_select = "select * from Photo where id_photo = %s limit 1;"
+    request_select = "select id_photo, id_plante, image_data, advice_title, advice from Photo where id_photo = %s limit 1;"
     cursor = connection.cursor()
     cursor.execute(request_select, (id))
-    image = cursor.fetchone()
-    if image is None:
+    rowImage = cursor.fetchone()
+    if rowImage is None:
         raise HTTPException(status_code=404, detail="Photo inexistante")
- 
+    
+    image = DBImage(id_photo = rowImage[0], id_plante = rowImage[1], data = rowImage[2],advice="", advice_title="")#, advice_title = rowImage[3], advice = rowImage[4]
+    if not rowImage[3] is None and not rowImage[4] is None:
+        image.advice = rowImage[3]
+        image.advice_title = rowImage[4]
+
     cursor.close()
     return image
