d9de68e01e3dd338333e964e00c89026c06e231b
ViTj4 <remi.ryckebusch.pro@gmail.com>
2023-02-20 16:48:43 +0100

    clear commentary on HEADERS.JS

--- MSPR-BDD-B3-WebApp/front/src/components/common/Header.js
+++ MSPR-BDD-B3-WebApp/front/src/components/common/Header.js
@@ -25,30 +25,7 @@ const Header = () => {
                 <Dropdown.Item as={Link} to="/AddAdvice">Ajouter un conseil</Dropdown.Item>  
                 </DropdownButton>  
                 </Container>  
-             </div>  
-
-                {/* <div style={{margin: "0"}}>
-                    <img src={logo} alt="logo" style={{width: "25%", height: "25%" }}/>
-                </div> */}
-                {/* <div className="d-flex align-items-center" >
-                    
-                    <Link to="/Map" className='btn btn-success m-1' >Garder une plante </Link>
-                    <Link to="*" className='btn btn-success'>localiser une plante </Link>
-                    <Link to="/login" className="nav-link btn btn-success"> Login </Link>
-                    <Link to="/Map" className="nav-link btn btn-success">Map</Link>
-                    <Link to="/Register" className="nav-link btn btn-success">Sign-In</Link>
-                    <Link to="/UserProfil" className="nav-link btn btn-success">UserProfil</Link>
-                    <Link to="/DevGa" className="nav-link btn btn-success">Work-In-Progress</Link>
-                    <Link to="/AddAdvice" className="nav-link btn btn-success">Ajouter un conseil</Link>
-                </div>
-                <ul className="navbar-nav mr-auto">
-                    <li></li>
-                    <li></li>
-                    <li></li>
-                    <li></li>
-                    <li></li>
-                </ul> */}
-
+            </div>  
             </nav>
         </>
     )
