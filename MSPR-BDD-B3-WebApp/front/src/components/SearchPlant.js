49a2a0eeb408488a93a8c6d7e4470aa32939e248
Rémi RYCKEBUSCH <remi.ryckebusch.pro@gmail.com>
2023-03-12 22:02:45 +0100

    FIX BUG DES CARDS QUI SORTENT DE L'ECRAN

--- MSPR-BDD-B3-WebApp/front/src/components/SearchPlant.js
+++ MSPR-BDD-B3-WebApp/front/src/components/SearchPlant.js
@@ -3,6 +3,7 @@ import axios from "axios";
 import Card from 'react-bootstrap/Card';
 import ListGroup from 'react-bootstrap/ListGroup';
 import { useNavigate } from "react-router-dom";
+import '../styles/Searchplant.css';
 
 function SearchPlant() {
     const [plants, setPlants] = useState([]);
@@ -27,26 +28,52 @@ function SearchPlant() {
     }, []);
 
     return (
-        <div style={{ display: 'flex', justifyContent: 'center' }}>
-            <Card.Link style={{ marginBottom: "0px", display: "flex", justifyContent: "center", alignItems: "center" }} onClick={openPlanteRegister}>Enregistrer une plante</Card.Link>
-            {plants.map(plant => (
-            <>
-                <div key={plant.id_plante}>
-                    <Card style={{ width: '18rem' }}>
-                        <Card.Img variant="top" src={plant.image_data} />
-                        <Card.Body>
-                            <Card.Title style={{ marginBottom: "0px", display: "flex", justifyContent: "center", alignItems: "center" }}><h2>{plant.name}</h2></Card.Title>
-                        </Card.Body>
-                        <ListGroup className="list-group-flush">
+        <>
+            <div className=' d-flex align-items-center justify-content-center mt-5'>
+                <button className='btn btn-success mb-5' onClick={openPlanteRegister}>
+                    Enregistrer une plante
+                </button>
+            </div>
+            <div className='plants-container d-flex align-items-center justify-content-center'>
+                {plants.map((plant) => (
+                    <div key={plant.id_plante} style={{ margin: '10px' }}>
+                        <Card style={{ width: '18rem' }}>
+                            <Card.Img variant='top' src={plant.image_data} />
                             <Card.Body>
-                                <Card.Link style={{ marginBottom: "0px", display: "flex", justifyContent: "center", alignItems: "center" }} onClick={() =>{openPlante(plant.id_plante)}}>Selectionner cette plante</Card.Link>
+                                <Card.Title
+                                    style={{
+                                        marginBottom: '0px',
+                                        display: 'flex',
+                                        justifyContent: 'center',
+                                        alignItems: 'center',
+                                    }}
+                                >
+                                    <h2>{plant.name}</h2>
+                                </Card.Title>
                             </Card.Body>
-                        </ListGroup>
-                    </Card>
-                </div>
-            </>
-            ))}
-        </div>
+                            <ListGroup className='list-group-flush'>
+                                <Card.Body>
+                                    <Card.Link
+                                        style={{
+                                            marginBottom: '0px',
+                                            display: 'flex',
+                                            justifyContent: 'center',
+                                            alignItems: 'center',
+                                        }}
+                                        onClick={() => {
+                                            openPlante(plant.id_plante);
+                                        }}
+                                    >
+                                        Selectionner cette plante
+                                    </Card.Link>
+                                </Card.Body>
+                            </ListGroup>
+                        </Card>
+                    </div>
+                ))}
+            </div>
+
+        </>
     );
 }
 export default SearchPlant;
