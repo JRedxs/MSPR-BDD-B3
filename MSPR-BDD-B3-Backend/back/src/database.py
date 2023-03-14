994529caa929e9037feaf7e8aedaa11948c061b6
Vonkuro <vonkuro@gmail.com>
2023-03-14 20:48:47 +0100

    bdd in utf-8 certain

--- MSPR-BDD-B3-Backend/back/src/database.py
+++ MSPR-BDD-B3-Backend/back/src/database.py
@@ -5,10 +5,10 @@ import pymysql
 
 
 def mysql_local():
-    return pymysql.connect(host='localhost', user='admin', password='admin', db='Arosaje-db', port=3306)
+    return pymysql.connect(host='localhost', user='admin', password='admin', db='Arosaje_db', port=3306)
 
 def mysql_container():
-    return pymysql.connect(host='mysql-db-compose', user='admin', password='admin', db='Arosaje-db', port=3306)
+    return pymysql.connect(host='mysql-db-compose', user='admin', password='admin', db='Arosaje_db', port=3306)
 
 def mysql():
     mysql = ''
