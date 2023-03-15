da4c02574c60cfb128cf37169679bc3ab902bad9
Vonkuro <vonkuro@gmail.com>
2023-03-15 08:43:22 +0100

    hot fix : supression of useless routes

--- MSPR-BDD-B3-WebApp/front/src/components/common/Header.js
+++ MSPR-BDD-B3-WebApp/front/src/components/common/Header.js
@@ -14,29 +14,7 @@ const Header = () => {
     };
     return (
         <>
-            {/* <nav className="navbar navbar-expand-lg  bg-info  d-flex align-items-center justify-content-center" style={{borderRadius: '15px'}}>
-            <div style={{margin: "0"}}>
-                <Link to="/">
-                    <img src={logo} alt="logo" style={{width: "25%", height: "25%" }}/>
-                </Link>
-                    
-            </div>
-            <div className="drop-down-menu">  
-                <Container className='p-4'>  
-                 <DropdownButton size='md' variant="success" id="dropdown-basic-button" title="Menu">  
-                 <Dropdown.Item as={Link} to="/login">Login</Dropdown.Item>  
-                 <Dropdown.Item as={Link} to="/UserProfil">Profil utilisateur</Dropdown.Item>  
-                 <Dropdown.Item as={Link} to="/Register">Se créer un compte</Dropdown.Item>
-                 <Dropdown.Item as={Link} to="/Map">Garder une plante</Dropdown.Item>  
-                 <Dropdown.Item as={Link} to="/DevGa">Photo</Dropdown.Item> 
-                 <Dropdown.Item as={Link} to="/SearchPlant">Rechercher une plante</Dropdown.Item>
-                 <Dropdown.Item as={Link} to="/Plante">Plantes</Dropdown.Item>     
-                 <Dropdown.Item as={Link} to="/AddAdvice">Ajouter un conseil</Dropdown.Item> 
-                 <Dropdown.Item as={Link} to="/RegisterGarde">Enregistrer une garde</Dropdown.Item> 
-                </DropdownButton>  
-                </Container>  
-            </div>  
-            </nav> */}
+            
             <nav className="navbar navbar-expand-lg navbar-light shadow-lg">
                 <a class="navbar-brand" href="/" style={{ marginLeft: '20px', color: 'white' }}> <b>Arosa-Je </b></a>
                 <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
@@ -49,12 +27,6 @@ const Header = () => {
                         </li>
                         <li class="nav-item">
                             <a class="nav-link" href="/SearchPlant" style={{ color: 'white' }}>Rechercher une plante</a>
-                        </li>
-                        <li class="nav-item">
-                            <a class="nav-link" href="/AddAdvice" style={{ color: 'white' }}>Ajouter un conseil</a>
-                        </li>
-                        <li class="nav-item">
-                            <a class="nav-link" href="/RegisterGarde" style={{ color: 'white' }}>Enregistrer une garde</a>
                         </li>
                     </ul>
                 </div>
