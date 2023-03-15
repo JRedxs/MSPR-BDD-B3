2ef80e3dd7e4d274db8580c17e8853f2f32b59dd
ViTj4 <remi.ryckebusch.pro@gmail.com>
2023-03-15 15:06:57 +0100

    masquage change password

--- MSPR-BDD-B3-WebApp/front/src/components/Login.js
+++ MSPR-BDD-B3-WebApp/front/src/components/Login.js
@@ -50,9 +50,10 @@ const LoginPage = () => {
                                                     <label className="form-label" htmlFor="form2Example27">Password</label>
                                                 </div>
                                                 <div className="d-flex justify-content-center pt-1 mb-4">
-                                                    <button className="btn btn-success btn-lg btn-block" style={{ backgroundColor: '#8E685A ', color: 'white' }} type="submit" to="/RegisterPlante">Login</button>
+                                                    <button className="btn btn-success btn-lg btn-block m-1" style={{ backgroundColor: '#8E685A ', color: 'white' }} type="submit" to="/RegisterPlante">Login</button>
+                                                    {/* <button className="btn btn-success btn-lg btn-block m-1"  style={{ backgroundColor: '#8E685A ', color: 'white' }} to="/changePass">Forgot password?</button> */}
                                                 </div>
-                                                <a className="small text-muted" to="/changePass">Forgot password?</a>
+                                                
                                                 <p className="mb-5 pb-lg-2" style={{ color: "#393f81" }}>Pas encore de compte ? <a href="/Register" style={{ color: "#393f81" }}>Inscrivez vous ici</a></p>
                                             </form>
                                         </div>
