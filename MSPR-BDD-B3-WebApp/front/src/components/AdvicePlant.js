f469622ea2c72e2e5dec1ac1c7b9a05575a88d47
ViTj4 <remi.ryckebusch.pro@gmail.com>
2023-03-15 10:55:53 +0100

    Fix search plant + responsive add

--- MSPR-BDD-B3-WebApp/front/src/components/AdvicePlant.js
+++ MSPR-BDD-B3-WebApp/front/src/components/AdvicePlant.js
@@ -54,41 +54,34 @@ const AdvicePlant = () => {
 
   return (
     <>
-
-
-
-
       <div className="body">
         <div className="container">
-          <h1 className="d-flex justify-content-center mb-4" style={{ textAlign: 'center' }}>
-            <b> Conseils </b>
+          <h1 className="d-flex justify-content-center mb-5" style={{ textAlign: 'center' }}>
+            <b><u>Proposer un conseil</u>  </b>
           </h1>
           <div className="card card-login mx-auto blue-card shadow-lg bg-white rounded" style={{ borderRadius: "15px", border: "2px solid black", width: '50%', height: '35em', marginTop: 0, marginBottom: 0 }}>
             <div className="card-body mx-auto">
               <div className="d-flex justify-content-center margin-login-card" style={{ height: 1000 }}>
                 <form className="mx-auto" >
                   <div className="card-body mx-auto" style={{ textAlign: 'center' }}>
-                    {/* condition : si user est null, on affiche rien, sinon on affiche l'image... */}
                     {user && (<img src={user[1].image_data} alt="" style={{ width: "55%", borderRadius: '15px', margin: 'auto' }} />)}
                   </div>
-                  <div className="card-body mx-auto" style={{ textAlign: 'center' }}>
-                    <label className="card-body mx-auto" htmlFor="begining" style={{ color: 'black', fontSize: '20px' }}> <b>Titre du conseil : </b> </label>
-                    <input type="text" name="advice_title" value={advice.advice_title} onChange={handleInputChange} placeholder="Titre" />
+                  <div className="d-flex justify-content-lg-center">
+                    <div className="card-body mx-auto" style={{ textAlign: 'center' }}>
+                      <label className="card-body mx-auto" htmlFor="begining" style={{ color: 'black', fontSize: '20px' }}> <b>Titre du conseil : </b> </label>
+                      <input type="text" name="advice_title" value={advice.advice_title} onChange={handleInputChange} placeholder="Titre" />
+                    </div>
+                    <div className="card-body mx-auto" style={{ textAlign: 'center' }}>
+                      <label className="card-body mx-auto" htmlFor="begining" style={{ color: 'black', fontSize: '20px' }}> <b>Conseil : </b> </label>
+                      <input name="advice" value={advice.advice} onChange={handleInputChange} placeholder="Conseil" />
+                    </div>
                   </div>
-
-                  <div className="card-body mx-auto" style={{ textAlign: 'center' }}>
-                    <label className="card-body mx-auto" htmlFor="begining" style={{ color: 'black', fontSize: '20px' }}> <b>Conseil : </b> </label>
-                    <input name="advice" value={advice.advice} onChange={handleInputChange} placeholder="Conseil" />
-                  </div>
-
                   <div className=' d-flex align-items-center justify-content-center mt-5'>
                     <button className='btn btn-success mb-5' type="submit" onClick={handleSubmit}>
-                      Ajouter
+                      Ajouter votre conseil
                     </button>
                   </div>
                 </form>
-
-
               </div>
             </div>
           </div>
--- MSPR-BDD-B3-WebApp/front/src/components/SearchPlant.js
+++ MSPR-BDD-B3-WebApp/front/src/components/SearchPlant.js
@@ -30,50 +30,49 @@ function SearchPlant() {
 
     return (
         <>
-            <div className='plants-container d-flex align-items-center justify-content-center'>
-                {plants.map((plant) => (
-                    <div key={plant.id_plante} style={{ margin: '10px' }}>
-                        <Card style={{ width: '18rem' }}>
-                            <Card.Img variant='top' src={plant.image_data} />
-                            <Card.Body>
-                                <Card.Title
-                                    style={{
-                                        marginBottom: '0px',
-                                        display: 'flex',
-                                        justifyContent: 'center',
-                                        alignItems: 'center',
-                                    }}
-                                >
-                                    <h2>{plant.name}</h2>
-                                </Card.Title>
-                            </Card.Body>
-                            <ListGroup className='list-group-flush'>
+            <div className="body">
+                <div className="d-flex justify-content-center">
+                    <button className='btn btn-success btn-lg mb-5 '  onClick={openPlanteRegister}>Enregistrer une plante</button> 
+                </div>
+              
+                <div className='plants-container d-flex flex-wrap align-items-center justify-content-center'>
+                    {plants.map((plant) => (
+                        <div key={plant.id_plante} style={{ margin: '10px' }}>
+                            <Card style={{ width: '33rem' }}>
+                                <Card.Img variant='top' src={plant.image_data} />
                                 <Card.Body>
-                                    <Card.Link
+                                    <Card.Title
                                         style={{
                                             marginBottom: '0px',
                                             display: 'flex',
                                             justifyContent: 'center',
                                             alignItems: 'center',
                                         }}
-                                        onClick={() => {
-                                            openPlante(plant.id_plante);
-                                        }}
                                     >
-                                        Selectionner cette plante
-                                    </Card.Link>
+                                        <h2>{plant.name}</h2>
+                                    </Card.Title>
                                 </Card.Body>
-                            </ListGroup>
-                        </Card>
-                    </div>
-                ))}
+                                <ListGroup className='list-group-flush'>
+                                    <Card.Body>
+                                        <div className="d-flex justify-content-center">
 
+                                        
+                                        <Card.Link 
+                                            className="btn btn-success"
+                                            onClick={() => {
+                                                openPlante(plant.id_plante);
+                                            }}
+                                        >
+                                            Selectionner cette plante
+                                        </Card.Link>
+                                        </div>
+                                    </Card.Body>
+                                </ListGroup>
+                            </Card>
+                        </div>
+                    ))}
+                </div>
             </div>
-            <div className="center-align">
-                <button className='btn btn-success mb-5' onClick={openPlanteRegister}>Enregistrer une plante</button>
-            </div>
-
-
         </>
     );
 }
