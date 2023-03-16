0dc04f74d0b26f5981f79981f28197e8855dc0ee
JRedxs <98217327+JRedxs@users.noreply.github.com>
2023-03-16 10:51:42 +0100

    ajout: token lors de la création de compte + fichiers security et .env variabilisation de la secret, fix: modification des routes pour ajouter de la clareté

--- MSPR-BDD-B3-Backend/back/src/main.py
+++ MSPR-BDD-B3-Backend/back/src/main.py
@@ -3,17 +3,19 @@
 from fastapi import FastAPI, HTTPException
 from models import *
 from database import *
+import bcrypt
 from fastapi.middleware.cors import CORSMiddleware
-# from security import *
+from security import *
 # from oauth import *
 
 # Connexion à la base de données
 connection = MSQL
 
-# Initialisez l'application 
+# Initialisez l'application
 app = FastAPI()
 
-#not ideal
+pwd_context = CryptContext(schemes=["bcrypt"], deprecated="auto")
+# not ideal
 origins = [
     "http://localhost",
     "http://localhost:3000",
@@ -33,16 +35,35 @@ app.add_middleware(
     allow_headers=["*"],
 )
 
+
 def takeLatinTupleGetUtf8List(theTuple):
     newList = []
-    for n in range(len(theTuple)) :
+    for n in range(len(theTuple)):
         if isinstance(theTuple[n], str):
             newList.append(theTuple[n].encode('latin-1').decode('utf-8'))
         else:
             newList.append(theTuple[n])
     return newList
 
-@app.get("/users")
+
+@app.post("/token" ,summary="Création de personnes et ajout d'un accès token")
+def login(person: Person):
+    # Hash the password
+    hashed_password = pwd_context.hash(person.password)
+
+    # Save the user to the database
+    with connection.cursor() as cursor:
+        insert_query = "INSERT INTO Person (name, firstname, pwd, email, phone,id_role) VALUES (%s, %s, %s, %s,%s,%s)"
+        cursor.execute(insert_query, (person.name, person.firstname, hashed_password, person.email, person.phone,person.id_role))
+        connection.commit()
+
+    # Generate JWT payload
+    access_token_expires = timedelta(minutes=ACCESS_TOKEN_EXPIRE_MINUTES)
+    access_token = create_access_token(data={"sub": person.email}, expires_delta=access_token_expires)
+
+    return {"Ajout": "Avec succès", "access_token": access_token, "token_type": "bearer"}
+
+@app.get("/users" ,summary="Récupération des personnes en fonction de leur email & mot de passe")
 def get_user(email: str, password: str):
     
     with connection.cursor() as cursor:
@@ -61,9 +82,29 @@ def get_user(email: str, password: str):
         except:
             cursor.close()
             raise HTTPException(status_code=500, detail="Database connection error !")
+        
+@app.get("/users_all" ,summary="Récupération de toutes les utilisateurs")
+def get_user():
+    
+    with connection.cursor() as cursor:
+        try:
+            sql = "SELECT * FROM Person"
+            cursor.execute(sql)
+            results = cursor.fetchall()
+            users = []
+            for result in results:
+                row = takeLatinTupleGetUtf8List(result)
+                users.append({"id_person": row[0], "name": row[1], "firstname": row[2], "pwd": row[3], "email": row[4], "phone": row[5], "latitude": row[6], "longitude": row[7], "id_role": row[8]})
+            if users:
+                return {"User": users}
+            else:
+                raise HTTPException(status_code=400, detail="Incorrect email or password")
+        except:
+            cursor.close()
+            raise HTTPException(status_code=500, detail="Database connection error !")
 
 
-@app.get("/users/{user_id}")
+@app.get("/users/{user_id}", summary="Récupération en fonction de l'id utilisateur" )
 def get_user_by_id(user_id: int):
     
     with connection.cursor() as cursor:
@@ -84,19 +125,19 @@ def get_user_by_id(user_id: int):
 
 
 
-@app.post("/persons", summary="Création de personnes et ajout d'un accès token")
-async def add_user(person: Person):
-    cursor = connection.cursor()
-    cursor.execute("SELECT * FROM Person WHERE email=%s", (person.email,))
-    if cursor.fetchone() is not None:
-        raise HTTPException(status_code=409, detail="Email déjà existants")
+# @app.post("/persons", summary="")
+# async def add_user(person: Person):
+#     cursor = connection.cursor()
+#     cursor.execute("SELECT * FROM Person WHERE email=%s", (person.email,))
+#     if cursor.fetchone() is not None:
+#         raise HTTPException(status_code=409, detail="Email déjà existants")
     
-    insert_query = "INSERT INTO Person (name, firstname, pwd, email, phone,id_role) VALUES (%s, %s, %s, %s,%s,%s)"
-    cursor.execute(insert_query, (person.name, person.firstname, person.password, person.email, person.phone,person.id_role))
-    connection.commit()
+#     insert_query = "INSERT INTO Person (name, firstname, pwd, email, phone,id_role) VALUES (%s, %s, %s, %s,%s,%s)"
+#     cursor.execute(insert_query, (person.name, person.firstname, person.password, person.email, person.phone,person.id_role))
+#     connection.commit()
 
-    cursor.close()
-    return {"message": "Ajout avec succès"}
+#     cursor.close()
+#     return {"message": "Ajout avec succès"}
 
 # @app.post('/login')
 # def login(request: OAuth2PasswordRequestForm = Depends()):
@@ -107,7 +148,7 @@ async def add_user(person: Person):
 #     return {"access_token": access_token, "token_type": "bearer"}
 
 
-@app.post("/image")
+@app.post("/image" , summary="Insertion d'une image")
 async def register_image(image: NewImage):
     request_select = "select id_plante from Plante where id_plante = %s limit 1;"
     request_insert = "insert into Photo (image_data, id_plante) values(%s,%s);"
@@ -122,7 +163,7 @@ async def register_image(image: NewImage):
     cursor.close()
     return {"message": "Photo enregistrée"}
     
-@app.get("/image/{id}", response_model=DBImage)
+@app.get("/image/{id}", response_model=DBImage, summary="Récupération d'une image en fonction de l'id")
 def send_image(id):
     request_select = "select id_photo, id_plante, image_data, advice_title, advice from Photo where id_photo = %s limit 1;"
     cursor = connection.cursor()
@@ -142,7 +183,7 @@ def send_image(id):
     return image
 
 
-@app.put("/advices")
+@app.put("/advices" , summary="Modification d'un titre conseil")
 def create_advice(advice: dict):
     
     with connection.cursor() as cursor:
@@ -158,7 +199,7 @@ def create_advice(advice: dict):
         
 
 
-@app.get("/advices")
+@app.get("/advices" , summary="Récupération des photos")
 def get_advices():
     with connection.cursor() as cursor:
         try:
@@ -179,7 +220,7 @@ def get_advices():
             raise HTTPException(status_code=500, detail="Database connection error !")
     
 
-@app.get("/plant")
+@app.get("/plant" , summary="Récupération des plantes")
 def get_plants():
     with connection.cursor() as cursor:
         try:
@@ -211,7 +252,7 @@ def get_plants():
             raise HTTPException(status_code=500, detail="Database connection error !")
 
 #maybe broken
-@app.get("/plants")
+@app.get("/plants" , summary="Récupération des garde de plantes")
 def get_info_plants():
     
     with connection.cursor() as cursor:
@@ -259,7 +300,7 @@ def get_info_plants():
 
 
 
-@app.post("/plants_garde")
+@app.post("/plants_garde" , summary="Insertion des gardes")
 def add_garde(garde: Garde):
     
     with connection.cursor() as cursor:
@@ -278,7 +319,7 @@ def add_garde(garde: Garde):
         
 
 
-@app.post("/plante")
+@app.post("/plante" , summary="Insertion des plantes")
 async def register_plante(plante : PlantToCreate):
     cursor = connection.cursor()
     sql = "Insert into Plante (id_person, name, number, road_first, road_second, town, postal_code, latitude, longitude) values (%s, %s, %s, %s, %s, %s, %s, %s, %s);"
@@ -293,7 +334,7 @@ async def register_plante(plante : PlantToCreate):
     cursor.close()
     return {"message": "Plante enregistrée", "id_plante": result[0]}
 
-@app.get("/plant/{id_plante}")
+@app.get("/plant/{id_plante}" , summary="Récupération des plantes en fonction de son id")
 def get_plant_by_id(id_plante: int):
     
     with connection.cursor() as cursor:
@@ -311,7 +352,7 @@ def get_plant_by_id(id_plante: int):
             cursor.close()
             return {"Plante inexistante"}
     
-@app.put("/garde/{id_garde}")
+@app.put("/garde/{id_garde}" , summary="Récupération des gardes en fonction de son id")
 def put_garde_by_id(id_garde: int, id_person: int):
     with connection.cursor() as cursor:
         # Recherche du garde existant dans la base de données
@@ -332,7 +373,7 @@ def put_garde_by_id(id_garde: int, id_person: int):
         return "Garde mis à jour", 200
 
 
-@app.get("/all_gardes")
+@app.get("/all_gardes" , summary="Récupération des gardes")
 def get_all_gardes():
     
     with connection.cursor() as cursor:
@@ -350,7 +391,7 @@ def get_all_gardes():
             return {"Error message": str(error)}
     
 
-@app.get("/plantandgallery/{id_plante}")
+@app.get("/plantandgallery/{id_plante}" , summary="Récupération des photos de plantes en fonction de leur id")
 def get_plant_photos_by_id(id_plante: int):
     
     with connection.cursor() as cursor:
--- MSPR-BDD-B3-Backend/back/src/security.py
+++ MSPR-BDD-B3-Backend/back/src/security.py
@@ -3,46 +3,50 @@
 from passlib.context import CryptContext
 import os
 from datetime import datetime, timedelta
-from typing import Union, Any
+from typing import Union, Any,Optional
 from jose import jwt
+from database import *
+from fastapi import Depends, FastAPI, HTTPException, Request
+from fastapi.security import HTTPBearer, HTTPAuthorizationCredentials
+from datetime import datetime, timedelta
+from dotenv import load_dotenv
 
-# connection = MSQL
+load_dotenv()
 
-# SECRET_KEY = os.getenv('SECRET')
-# ALGORITHM = os.getenv('ALGORITHM')
-# ACCES_TOKEN_EXPIRE_MINUTES = 30
+connection = MSQL
 
-# def create_access_token(data: dict):
-#     to_encode = data.copy()
-#     expire = datetime.utcnow() + timedelta(minutes=ACCES_TOKEN_EXPIRE_MINUTES)
-#     to_encode.update({"expiration_date": expire})
-#     encoded_jwt = jwt.encode(to_encode, SECRET_KEY,algorithm=ALGORITHM)
-#     return encoded_jwt
-
-# # def verify_token(token: str, credentials_exception):
-# #     try:
-# #         payload = jwt.decode(token,SECRET_KEY,algorithms=[ALGORITHM])
-# #         username: str = payload.get("sub")
-# #         if username is None:
-# #             raise credentials_exception
-# #         token_data = TokenData(username=username)
-# #     except JWTError:
-# #         raise credentials_exception
+SECRET_KEY = os.getenv('SECRET')
+ALGORITHM = os.getenv('ALGORITHM')
+ACCESS_TOKEN_EXPIRE_MINUTES = 30
 
 
-# def verify_token(token: str, credentials_exception):
-#     try:
-#         payload = jwt.decode(token, SECRET_KEY, algorithms=[ALGORITHM])
-#         username: str = payload.get("sub")
-#         if username is None:
-#             raise credentials_exception
-#         token_data = TokenData(username=username)
-#     except JWTError:
-#         raise credentials_exception
+def create_access_token(data: dict, expires_delta: timedelta) -> str:
+    to_encode = data.copy()
+    expire = datetime.utcnow() + expires_delta
+    to_encode.update({"exp": expire})
+    encoded_jwt = jwt.encode(to_encode, SECRET_KEY, algorithm=ALGORITHM)
+    return encoded_jwt
 
-#     cursor = connection.cursor(dictionary=True)
-#     cursor.execute("SELECT * FROM users WHERE username=%s", (token_data.username,))
-#     user = cursor.fetchone()
-#     if not user:
-#         raise credentials_exception
-#     return user
+class BearerAuth(HTTPBearer):
+    def __init__(self, auto_error: bool = True):
+        super(BearerAuth, self).__init__(auto_error=auto_error)
+
+    def __call__(self, request: Request) -> Optional[str]:
+        credentials: HTTPAuthorizationCredentials = super(BearerAuth, self).__call__(request)
+        if credentials:
+            if not credentials.scheme == "bearer":
+                raise HTTPException(status_code=403, detail="Invalid authentication scheme")
+            if not self.verify_jwt(credentials.credentials):
+                raise HTTPException(status_code=403, detail="Invalid token")
+            return credentials.credentials
+        else:
+            raise HTTPException(status_code=403, detail="Invalid authorization credentials")
+
+    def verify_jwt(self, jwt_token: str) -> bool:
+        try:
+            payload = jwt.decode(jwt_token, SECRET_KEY, algorithms=ALGORITHM)
+            return payload
+        except jwt.ExpiredSignatureError:
+            raise HTTPException(status_code=403, detail="Token has expired")
+        except jwt.InvalidTokenError:
+            return False
