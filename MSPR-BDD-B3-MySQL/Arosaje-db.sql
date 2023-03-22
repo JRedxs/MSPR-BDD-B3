1f4bcd8eb00c76a73fc0457ffb7b9bada174d8a0
Vonkuro <vonkuro@gmail.com>
2023-03-22 20:01:16 +0100

    Update du jeu de données avec des hashs de mots de passe

--- MSPR-BDD-B3-MySQL/Arosaje-db.sql
+++ MSPR-BDD-B3-MySQL/Arosaje-db.sql
@@ -60,7 +60,11 @@ CREATE TABLE Photo(
 
 INSERT INTO Role Values(1,'botaniste'),(2,'user'),(3,'Hybride');
 
-INSERT INTO Person(id_person, name, firstname, pwd, email, phone, latitude, longitude, id_role) VALUES (1,'Ryckebusch','Rémi','test','remi@remi.fr','0600000001',1.0,2.0,1),(2,'MonkeyD','Luffy','viande','luffy@zoro.fr','0600000001',2.0,3.0,2),(3,'Napoleon','Bonaparte','Waterloo','Waterloo@1801.fr','0600000001',2.0,3.0,3);
+# Passwords for developpment and tests
+# Rémi the botaniste : remi@remi.fr T@st96745
+# Luffy the client : luffy@zoro.fr Vi@nde159357
+# Bonaparte the botaniste and client : Waterloo@1801.fr W@terloo852963
+INSERT INTO Person(id_person, name, firstname, pwd, email, phone, latitude, longitude, id_role) VALUES (1,'Ryckebusch','Rémi','$2b$12$PW6IJupoyXR39ica/ek9suxMbDLJc7eBDBi7rBnah4BAAGb/3oWRm','remi@remi.fr','0600000001',1.0,2.0,1),(2,'MonkeyD','Luffy','$2b$12$21CA8C9iOJ7HMlZymbXNjO.v.MdxsteWrEY.7HrxoIZD5sLtKR1QK','luffy@zoro.fr','0600000001',2.0,3.0,2),(3,'Napoleon','Bonaparte','$2b$12$OJMNokl8U62TNRWZVF8q2.1jo6XBs02orx.gUX5VQXjGijlTBy6ue','Waterloo@1801.fr','0600000001',2.0,3.0,3);
 
 INSERT INTO Plante(id_plante, name, number, road_first, road_second, town, postal_code, latitude, longitude, id_person) VALUES (1,'Orchidee',5,'rue napoleon','','Dunkerque',59140,5.0,5.1,1),(2,'Rose',7,'Rue de test','','Lille',59000,1.0,3.1,2),(3,'Tulipe',13,'rue des elements','appartement 7','Marseille',13000,1.0,3.1,3);
 
