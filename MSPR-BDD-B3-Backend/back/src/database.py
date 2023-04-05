867469457307d8cb75e9a4d02e1f4b7aa5b3c8c8
JRedxs <98217327+JRedxs@users.noreply.github.com>
2023-04-05 13:54:14 +0200

    fix: variable d'env

--- MSPR-BDD-B3-Backend/back/src/database.py
+++ MSPR-BDD-B3-Backend/back/src/database.py
@@ -30,7 +30,7 @@ print('IIIIIIIIIIIIIIIIII', REACT_APP_API_URL)
 
 
 def mysql_local():
-    return pymysql.connect(host=HOST_LOCAL, user=USER_LOCAL, password=PASSWORD_LOCAL, db=DB_LOCAL, port=3306, charset='utf8')
+    return pymysql.connect(host=HOST_LOCL, user=USER_LOCAL, password=PASSWORD_LOCAL, db=DB_LOCAL, port=3306, charset='utf8')
 
 
 
