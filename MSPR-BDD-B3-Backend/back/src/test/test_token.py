5ca0a21611f0870eba06b5bfc8b2f35581fef11d
JRedxs <98217327+JRedxs@users.noreply.github.com>
2023-04-07 11:19:00 +0200

    fix: test_token suppression des lib obsolètes

--- MSPR-BDD-B3-Backend/back/src/test/test_token.py
+++ MSPR-BDD-B3-Backend/back/src/test/test_token.py
@@ -1,25 +1,22 @@
-import pytest
+import pytest,jwt,time
 from fastapi import HTTPException
 from main_test import decoded_jwt,create_access_token
-import jwt
-from fastapi import HTTPException
-import time
-from datetime import timedelta
 
 
 
+# Test create access token
 def test_create_access_token():
     token = create_access_token("1",{"name" :"John", "firstname" :"Doe"})
     assert isinstance(token, str)
 
 
-# Test a valid token
+# Test valid token
 def test_decode_jwt():
     token = jwt.encode({'user': 'John'}, 'test_key', 'HS256')
     decoded_token = decoded_jwt(token)
     assert decoded_token == {'user': 'John'}
 
-
+# Test expired token
 def test_decode_jwt_expired():
     token = jwt.encode({'exp': time.time() - 3600 }, 'test_key', 'HS256')
     with pytest.raises(HTTPException) as error:
@@ -27,7 +24,8 @@ def test_decode_jwt_expired():
     
     assert error.value.status_code == 403
     assert error.value.detail == "Token has expired"
-        
+
+# Test wrong token
 def test_decode_wrong_jwt():
 
     token = "invalid.token"
