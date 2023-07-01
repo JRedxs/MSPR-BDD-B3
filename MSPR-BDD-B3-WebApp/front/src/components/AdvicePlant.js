0c05bce56511bee06cb9e3c3716aa33465f81139
McLouffy <119299314+McLouffy@users.noreply.github.com>
2023-07-01 14:17:27 +0200

    form addAdvice

--- MSPR-BDD-B3-WebApp/front/src/components/AdvicePlant.js
+++ MSPR-BDD-B3-WebApp/front/src/components/AdvicePlant.js
@@ -1,18 +1,30 @@
-import React, { useState, useEffect } from "react";
-import { useNavigate } from "react-router-dom";
-import axios from "axios";
-//import '../styles/Advice.css'
+import { useState, useEffect } from 'react';
+import { useNavigate } from 'react-router-dom';
+import axios from 'axios';
+import {
+  Box,
+  Button,
+  Center,
+  FormControl,
+  FormErrorMessage,
+  FormLabel,
+  Input,
+  Text,
+  useToast,
+  Image
+} from '@chakra-ui/react';
 
 const AdvicePlant = () => {
   const id_plante = Number(sessionStorage.getItem('plante'));
   const id_photo = Number(sessionStorage.getItem('photo'));
   const baseUrl = process.env.REACT_APP_API_URL;
 
-  const [advice, setAdvice] = useState({ advice_title: "", advice: "", id_photo: id_photo });
+  const [advice, setAdvice] = useState({ advice_title: '', advice: '', id_photo: id_photo });
   const [user, setUser] = useState(null);
 
   const navigate = useNavigate();
   const [reloadPlante, setReloadPlante] = useState(false);
+  const toast = useToast();
 
   const handleInputChange = (event) => {
     const { name, value } = event.target;
@@ -22,7 +34,7 @@ const AdvicePlant = () => {
   useEffect(() => {
     const fetchData = async () => {
       try {
-        const accessToken = window.sessionStorage.getItem("access_token");
+        const accessToken = window.sessionStorage.getItem('access_token');
         const response_user = await axios.get(`${baseUrl}/plantandgallery/${id_plante}`, {
           headers: {
             Authorization: `Bearer ${accessToken}`,
@@ -31,14 +43,14 @@ const AdvicePlant = () => {
         const userData = response_user.data[0].Plante;
         setUser(userData);
         console.log(userData);
-        sessionStorage.setItem("user_plante", JSON.stringify(userData));
+        sessionStorage.setItem('user_plante', JSON.stringify(userData));
 
         // Mettre à jour les conseils dans l'état advice
         if (userData && userData[id_photo]) {
           setAdvice({
             ...advice,
-            advice_title: userData[id_photo].advice_title || "",
-            advice: userData[id_photo].advice || "",
+            advice_title: userData[id_photo].advice_title || '',
+            advice: userData[id_photo].advice || '',
           });
         }
       } catch (error) {
@@ -51,7 +63,7 @@ const AdvicePlant = () => {
   const handleSubmit = async (event) => {
     event.preventDefault();
     try {
-      const accessToken = window.sessionStorage.getItem("access_token");
+      const accessToken = window.sessionStorage.getItem('access_token');
       const response = await axios.put(`${process.env.REACT_APP_API_URL}/advices`, advice, {
         headers: {
           Authorization: `Bearer ${accessToken}`,
@@ -64,45 +76,70 @@ const AdvicePlant = () => {
       navigate(`/Plante/${id_plante}`);
     } catch (error) {
       console.error(error);
+      toast({
+        title: 'Erreur',
+        description: "Une erreur s'est produite lors de l'ajout du conseil.",
+        status: 'error',
+        duration: 3000,
+        isClosable: true,
+      });
     }
   };
 
   return (
-    <>
-      <div className="body">
-        <div className="container">
-          <h1 className="d-flex justify-content-center mb-5" style={{ textAlign: 'center' }}>
-            <b><u>Proposer un conseil</u>  </b>
-          </h1>
-          <div className="card card-login mx-auto blue-card shadow-lg bg-white rounded" style={{ borderRadius: "15px", border: "2px solid black", width: '50%', height: '35em', marginTop: 0, marginBottom: 0 }}>
-            <div className="card-body mx-auto">
-              <div className="d-flex justify-content-center margin-login-card" style={{ height: 1000 }}>
-                <form className="mx-auto" >
-                  <div className="card-body mx-auto" style={{ textAlign: 'center' }}>
-                    {user && (<img src={user[1].image_data} alt="" style={{ width: "55%", borderRadius: '15px', margin: 'auto' }} />)}
-                  </div>
-                  <div className="d-flex justify-content-lg-center">
-                    <div className="card-body mx-auto" style={{ textAlign: 'center' }}>
-                      <label className="card-body mx-auto" htmlFor="begining" style={{ color: 'black', fontSize: '20px' }}> <b>Titre du conseil : </b> </label>
-                      <input type="text" name="advice_title" value={advice.advice_title} onChange={handleInputChange} placeholder="Titre" />
-                    </div>
-                    <div className="card-body mx-auto" style={{ textAlign: 'center' }}>
-                      <label className="card-body mx-auto" htmlFor="begining" style={{ color: 'black', fontSize: '20px' }}> <b>Conseil : </b> </label>
-                      <input name="advice" value={advice.advice} onChange={handleInputChange} placeholder="Conseil" />
-                    </div>
-                  </div>
-                  <div className=' d-flex align-items-center justify-content-center mt-5'>
-                    <button className='btn btn-success mb-5' type="submit" onClick={handleSubmit}>
-                      Ajouter votre conseil
-                    </button>
-                  </div>
-                </form>
-              </div>
-            </div>
-          </div>
-        </div>
-      </div>
-    </>
+    <Center py="5">
+      <Box
+        w="20%"
+        p="6"
+        bg="white"
+        rounded="lg"
+        boxShadow="lg"
+        border="1px solid black"
+      >
+        <Text textAlign="center" fontSize="2xl" fontWeight="bold" mb="8">
+          Proposer un conseil
+        </Text>
+        <Box mx="auto" maxW="500px">
+  {user && (
+    <Center mb="6">
+      <Image src={user[1].image_data} alt="" w="55%" rounded="md" />
+    </Center>
+  )}
+  <form>
+    <FormControl mb="4">
+      <FormLabel htmlFor="advice_title">Titre du conseil :</FormLabel>
+      <Input
+        id="advice_title"
+        name="advice_title"
+        type="text"
+        placeholder="Titre"
+        value={advice.advice_title}
+        onChange={handleInputChange}
+        size="sm"
+      />
+    </FormControl>
+    <FormControl mb="4">
+      <FormLabel htmlFor="advice">Conseil :</FormLabel>
+      <Input
+        id="advice"
+        name="advice"
+        type="text"
+        placeholder="Conseil"
+        value={advice.advice}
+        onChange={handleInputChange}
+        size="sm"
+      />
+    </FormControl>
+    <Center mt="8">
+      <Button colorScheme="green" onClick={handleSubmit} size="sm">
+        Ajouter votre conseil
+      </Button>
+    </Center>
+  </form>
+</Box>
+
+      </Box>
+    </Center>
   );
 };
 
